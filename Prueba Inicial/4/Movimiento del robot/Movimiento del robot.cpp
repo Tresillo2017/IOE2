213bc1b8275bbc0c08c3615df8ff304db72f0e79
Tresillocrack <tresillo2017@gmail.com>
2021-12-04 09:52:13 +0100

    4,5

---
+++ Prueba Inicial/4/Movimiento del robot/Movimiento del robot.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+// La letra "U" significa que Tony se mueve hacia arriba: de (x,y) a (x,y+1).
+// La letra "R" significa que Tony se mueve hacia la derecha : de(x, y) a(x + 1, y).
+// La letra "D" significa que Tony se mueve hacia abajo : de(x, y) a(x, y−1).
+// La letra "L" significa que Tony se mueve hacia la izquierda : de(x, y) a(x−1, y).
+using namespace std;
+int main()
+{
+	int movimiento;
+	int x, y = 0;
+    cin >> movimiento;
+	if (movimiento = U)
+	{
+		cin >> x + 1;
+	}	
+	if (movimiento = R)
+	{
+		cin >> x+1
+	}
+	if (movimiento = D)
+	{
+		cin >> y-1
+	}
+	if (movimiento = L)
+	{
+		cin >> x-1
+	}
+	cout << x;
+	cout << y;
+}
---
+++ Prueba Inicial/5/Pintura Original/Pintura Original.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int 1, 2, 3, 4;
+    cin >> 1;
+    cin >> 2;
+    cin >> 3;
+    cin >> 4;
+    if (1)
+}
+
