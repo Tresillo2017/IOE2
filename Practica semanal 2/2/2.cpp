cdf60ef739c580603d1461f479a449f430f8705b
Tresillocrack <tresillo2017@gmail.com>
2021-12-16 12:24:47 +0100

    2

---
+++ Practica semanal 2/2/2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numero, resultado;
+	cin >> numero;
+	resultado = numero % 2;
+	if (numero = 0) 
+	{
+		cout << "EVEN";
+	}
+    else //(numero == 1)
+	{
+		cout << "ODD"; 
+	}
+}
