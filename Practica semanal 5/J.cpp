d1d73f3fc1699c81202770d7f9d6dd9185c8be2f
Tresillocrack <tresillo2017@gmail.com>
2022-01-19 21:29:11 +0100

    j

--- Practica semanal 5/J.cpp
+++ Practica semanal 5/J.cpp
@@ -5,21 +5,45 @@ using namespace std;
 int main()
 {
     int contador = 0;
-    string s, primera, segunda;
+    string s;
+    char test[100];
+    char primera[50], segunda[50];
     cin >> s;
-    for (int i = 0; i < s.length()/2-1; i++)
-        primera[i] = s[i];
 
+
+
+    int j = 0;
+    for (int i = s.length()/2; i < s.length(); i++)
+    {
+        test[j] = s[i];
+        cout << test[j];
+        j++;
+    }
+
+    for (int i = 0; i < s.length()/2; i++)
+    {
+            test[j] = s[i];
+            j++;
+            cout << test[j] << endl;
+    }
+
+    // cout << primera << endl;
+    /* for (int i = 0; i < s.length()/2; i++)
+    {
+            primera[i] = s[i];
+            cout << primera[i] << endl;
+    }
     int j = 0;
 
     for (int i = s.length()/2; i < s.length(); i++)
     {
         segunda[j] = s[i];
-        j++;
         cout << segunda[j];
+        j++;
     }
-
-    cout << segunda << endl;
+    */
+    // cout << segunda;
+    // cout << primera << endl;
 }   
 
 
