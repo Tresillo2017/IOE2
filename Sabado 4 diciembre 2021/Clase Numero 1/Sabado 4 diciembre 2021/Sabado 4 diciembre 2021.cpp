7406332d234228fdb450d2ce3d864318c37a2a0b
Tresillocrack <tresillo2017@gmail.com>
2022-02-05 14:22:19 +0100

    .

---
+++ Sabado 4 diciembre 2021/Clase Numero 1/Sabado 4 diciembre 2021/Sabado 4 diciembre 2021.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; 
+
+
+// int main() {
+//	int actual_year = 2021;
+//	int year_born; 
+//	cin >> year_born;
+//	int year_old = actual_year - year_born;
+//	cout << (year_old) << endl;
+
+//}
+
+int main()
+{
+	cout << 2021 << endl;
+}
+
